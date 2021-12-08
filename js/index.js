35f0973394947bb6c0369143007fa66481c070bc
lqh <1796646806@qq.com>
2021-12-08 16:07:30 +0800

    提交 一小部分

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
+//常规基础
+
+// 一个变量如果没有进行初始化（只声明，不赋值），那么这个变量中存储的值是undefined
+
+// ES6 常量
+// const 常量名称 = 常量取值;
+
+//ES5  变量
+var name; // 定义一个名为 name 的变量。name是变量名。
+
+//ES6
+const name; // 定义一个常量
+let age; // 定义一个变量
 
 
-function Vip(str) {
-    console.log(str);
-}
 
- 
+
+// 普通弹出
+alert('我是第三种引用!  外链式');
+
+// 弹出带确认取消
+var result = confirm('第三种方式');
+console.log(result);
+
+// 带输入   prompt()语句中，用户不管输入什么内容，都是字符串。
+var a = prompt('请随便输入点什么东西吧');
+console.log(a);
+
+
+// function Vip(str) {
+//     console.log(str);
+// }
+
